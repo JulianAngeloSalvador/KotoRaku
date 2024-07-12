37473a572161d00c2e678b2f580801282e834d10
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-12 09:55:22 +0800

    feat: added classes for typography, transition dur & fluid widths + utilized tw plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 import { createThemes } from "tw-colors";
+import defaultTheme from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -16,6 +18,20 @@ const config: Config = {
       bold: "700",
       extrabold: "900",
     },
+    fontSize: {
+      "special-headline": "var(--special-fs)",
+      "special-subheadline": "var(--special-sub-fs)",
+      headline: "clamp(3rem, 3vw + 1rem, 4.5rem)",
+      subheadline: "clamp(2rem, 3vw + 1rem, 3rem)",
+      caption: "clamp(1.35rem, 2vw + 1rem, 1.5rem)",
+      "main-fs": "clamp(1.05rem, 2vw + 1rem, 1.225rem)",
+      small: "clamp(.8rem, 2vw + 1rem, 1.05rem)",
+    },
+    transitionDuration: {
+      fast: ".3s",
+      medium: ".5s",
+      slow: ".75s",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -26,8 +42,21 @@ const config: Config = {
         primary: "#141414",
         secondary: "#EBEBEB",
       },
+      fontFamily: {
+        russo: ["var(--font-russo)"],
+        urbanist: ["var(--font-urbanist)"],
+      },
+      width: {
+        "fluid-120": "min(120px, 100%)",
+        "fluid-1200": "min(1200px, 100%)",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("children", "&>*"),
+        addVariant("hocus", ["&:hover", "&:focus"]);
+    }),
+  ],
 };
 export default config;
