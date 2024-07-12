a37924b0b7b150e4c9be346dcd2fddf96e7412bb
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-12 14:52:41 +0800

    feat(ui/comps/cta): added style attribute prop for style flexibility

--- app/ui/components/Cta.tsx
+++ app/ui/components/Cta.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 interface CTA {
   className?: string;
   title: string;
+  style?: CSSProperties;
 }
-export default function Cta({ className, title }: CTA) {
+export default function Cta({ className, title, style }: CTA) {
   return (
     <button
       className={`bg-secondary  outline-secondary text-primary font-bold ${className} transition-all duration-fast ease-in-out`}
+      style={style}
     >
       {title}
     </button>
